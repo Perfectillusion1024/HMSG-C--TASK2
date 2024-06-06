b4d29701c0aa724bfae7d074ef7c4b3c35fe58a0
Wiley <30597891+Perfectillusion1024@users.noreply.github.com>
2024-06-06 17:17:48 +1000

    V0.2

---
+++ HolmesglenStudentManagementSystem/BLL/StudentBLL.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using HolmesglenStudentManagementSystem.DAL;
+using HolmesglenStudentManagementSystem.Models;
+
+namespace HolmesglenStudentManagementSystem.BLL
+{
+    public class StudentBLL
+    {
+        private readonly StudentDAL _studentDAL;
+
+        public StudentBLL()
+        {
+            _studentDAL = new StudentDAL();
+        }
+
+        public void CreateStudent(Student student)
+        {
+            _studentDAL.CreateStudent(student);
+        }
+
+        public Student GetStudentById(int studentId)
+        {
+            return _studentDAL.ReadStudent(studentId);
+        }
+
+        public List<Student> GetAllStudents()
+        {
+            return _studentDAL.ReadAllStudents();
+        }
+
+        public void UpdateStudent(Student student)
+        {
+            _studentDAL.UpdateStudent(student);
+        }
+
+        public void DeleteStudent(int studentId)
+        {
+            _studentDAL.DeleteStudent(studentId);
+        }
+    }
+}
