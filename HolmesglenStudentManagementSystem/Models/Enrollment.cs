c578cbec9c107eab87a4b832a6569da47d77f88c
Wiley <30597891+Perfectillusion1024@users.noreply.github.com>
2024-06-06 17:22:01 +1000

    @2-3

--- HolmesglenStudentManagementSystem/Models/Enrollment.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HolmesglenStudentManagementSystem.Models
-{
-    public class Enrollment
-    {
-        public int ID { get; set; }
-        public int StudentID_FK { get; set; }
-        public int SubjectID_FK { get; set; }
-    }
-}
