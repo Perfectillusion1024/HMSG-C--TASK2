fe0f8ae6dba1470e15d55703f25cdeab7a4727c6
Wiley <30597891+Perfectillusion1024@users.noreply.github.com>
2024-06-06 17:23:42 +1000

    @2-4

---
+++ HolmesglenStudentManagementSystem/BLL/EnrollmentBLL.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using HolmesglenStudentManagementSystem.DAL;
+using HolmesglenStudentManagementSystem.Models;
+
+namespace HolmesglenStudentManagementSystem.BLL
+{
+    public class EnrollmentBLL
+    {
+        private readonly EnrollmentDAL _enrollmentDAL;
+
+        public EnrollmentBLL()
+        {
+            _enrollmentDAL = new EnrollmentDAL();
+        }
+
+        public void CreateEnrollment(Enrollment enrollment)
+        {
+            _enrollmentDAL.CreateEnrollment(enrollment);
+        }
+
+        public Enrollment GetEnrollmentById(int id)
+        {
+            return _enrollmentDAL.ReadEnrollment(id);
+        }
+
+        public List<Enrollment> GetAllEnrollments()
+        {
+            return _enrollmentDAL.ReadAllEnrollments();
+        }
+
+        public void UpdateEnrollment(Enrollment enrollment)
+        {
+            _enrollmentDAL.UpdateEnrollment(enrollment);
+        }
+
+        public void DeleteEnrollment(int id)
+        {
+            _enrollmentDAL.DeleteEnrollment(id);
+        }
+    }
+}
